21305f79783080388d3ce83af732c7f6a0c502bc
Petteri Pinkkilä <petteri.pinkkila@outlook.com>
2024-02-25 17:58:24 +0200

    Commit before trying React

--- src/main/java/com/packt/bookstore/web/BookController.java
+++ src/main/java/com/packt/bookstore/web/BookController.java
@@ -1,5 +1,22 @@
 package com.packt.bookstore.web;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.packt.bookstore.domain.Book;
+import com.packt.bookstore.domain.BookRepository;
+
+@RestController
 public class BookController {
+    private final BookRepository repository;
+
+    public BookController(BookRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("/books")
+    public Iterable<Book> getBooks() {
+        return repository.findAll();
+    }
 
 }
